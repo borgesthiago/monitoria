ed7dc1c15318e6f165879fd5802d079ffa9dadbe
borgesthiago <borges_thiago@hotmail.com>
2020-08-25 21:17:54 -0300

    Aula 25-08-20

--- calcularNotas.php
+++ calcularNotas.php
@@ -1,15 +1,29 @@
-<?php 
+<?php
+session_start();
+if (!isset($_SESSION["msg"])) {
+  $_SESSION["msg"] = "";
+} if (!isset($_SESSION["media"])) {
+  $_SESSION["media"] = "";
+}
 include "head.php"; 
 include "nav.php";
+include_once "calcularNotas.php";
 ?>
+<div class="container-fluid">
 <form action="dao/calculaMedia.php" method="POST">
-  <div class="form-group">
+  <div class="form-group col-sm-2">
     <label for="text">Nota 1:</label>
     <input type="number" class="form-control" placeholder="Nota 1" name="nota1">
   </div>
-  <div class="form-group">
+  <div class="form-group col-sm-2">
     <label for="text">Nota 2:</label>
     <input type="number" class="form-control" placeholder="Nota 2" name="nota2">
   </div>
   <button type="submit" class="btn btn-primary">Calcular</button>
 </form>
+<?php 
+  echo $_SESSION["media"] . "<br>";
+  echo  $_SESSION["msg"];
+  session_destroy();
+  ?>
+  </div>
--- dao/calculaMedia.php
+++ dao/calculaMedia.php
@@ -1,8 +1,33 @@
 <?php
+session_start();
 include "../head.php"; 
 include "../nav.php";
+?>
+<div class="container-fluid">
+<?php
     $nota1 = $_POST['nota1'];
     $nota2 = $_POST['nota2'];
-    
-    echo "A média é: ". ($nota1 + $nota2)/2;
+
+    if (($nota1 < 0 or $nota1 > 10) or ($nota2 < 0 or $nota2 > 10)) {
+        $_SESSION['msg'] = "Valor não permitido!<br>". $nota1 . " e " . $nota2 . " A nota deve conter valores de zero a dez.";
+        header('Location: ../calcularNotas.php');
+    exit;
+    } elseif (empty($nota1)  or empty($nota2)) {
+        $_SESSION['msg'] = "O(s) campo(s) está(ão) vazio(s), por favor preencha o(s) campo(s)!";
+        header('Location: ../calcularNotas.php');
+    exit;
+    }
+
+    $media = ($nota1 + $nota2)/2;
+    $_SESSION["media"] = "Sua média é: " .$media;
+    if ($media < 5) {
+        $_SESSION["msg"] = "Você está Reprovado";
+    } elseif ($media >= 5 and $media < 7) {
+        $_SESSION["msg"] = "Você tem uma chance na VS";
+    } else {
+        $_SESSION["msg"] = "Você está Aprovado!";
+    }
+    header('Location: ../calcularNotas.php');
+    exit;
 ?>
+</div>
